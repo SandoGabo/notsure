fdb51aceab4baf69aad60bd5936b6caebc23e0f5
SandoGabo <104393330+SandoGabo@users.noreply.github.com>
2024-02-26 17:21:13 -0600

    Create anything.py

---
+++ anything.py
@@ -0,0 +1,15 @@
+studentanswer = input("Enter the student's answers: ")
+realanswers = input("Enter the correct answers: ") 
+
+counter = 0
+
+for i in range(len(studentanswer)): 
+  if studentanwer[i] == realanswer[i] 
+  counter = counter + 1
+  else counter == counter
+
+grade = [] 
+
+grade = (counter / len(studentanswer)) * 100
+
+print(f"This is the students grade: {grade}%" 
